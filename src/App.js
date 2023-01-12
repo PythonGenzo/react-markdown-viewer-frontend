1f0c995ff8fc44b711c250dab0fa2fcd3e1e5da4
PythonGenzo <sanprasanth94@gmail.com>
2023-01-12 20:05:01 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
 import './App.css';
+import ReactMarkdown from "react-markdown";
+import { useEffect, useState } from 'react';
+// import remarkGfm from 'remark-gfm';
+
+
+const MarkDown = `
+  # H1
+  ## H2
+  ### H3
+
+ 	
+  **bold text**
+
+  	
+  *italicized text*
+
+  
+  > blockquote
+
+  Ordered List	
+  1. First item
+  2. Second item
+  3. Third item
+
+  Unordered List	
+  - First item
+  - Second item
+  - Third item
+  
+  
+  	---
+
+  Link	[title](https://www.example.com)
+
+  Image	![alt text](image.jpg)
+
+  | Syntax | Description |
+| ----------- | ----------- |
+| Header | Title |
+| Paragraph | Text |
+
+
+Here's a sentence with a footnote. [^1]
+
+[^1]: This is the footnote.
+    `
+  
 
 function App() {
+
+  const [markdown, setMarkdown] = useState(MarkDown);
+
+  useEffect(() => {
+    fetch("Page1.md")
+      .then((res) => res.text())
+      .then((text) => setMarkdown(text));
+  }, []);
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+      {/* <ReactMarkdown children={markdown}  /> */}
+      <main>
+      <section className="markdown">
+        <textarea
+          className="input"
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+        ></textarea>
+        <article className="result">
+          <ReactMarkdown children={markdown}>
+            {/* {markdown} */}
+          </ReactMarkdown>
+        </article>
+      </section>
+    </main>
+    {/* <PageComponent /> */}
+
     </div>
   );
 }
+
+const PageComponent = () => {
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    fetch("Page1.md")
+      .then((res) => res.text())
+      .then((text) => setContent(text));
+  }, []);
+
+  return (
+    <div className="post">
+      <ReactMarkdown children={content} />
+    </div>
+  );
+};
+
 
 export default App;
